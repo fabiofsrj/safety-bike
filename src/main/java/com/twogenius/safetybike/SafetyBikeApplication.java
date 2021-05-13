781aa4b3001c5a3ca8bc60ec02b6f99a6e362047
unknown <fabiofsrj@gmail.com>
2021-05-12 22:24:07 -0300

    criando estrutura inicial do projeto

---
+++ src/main/java/com/twogenius/safetybike/SafetyBikeApplication.java
@@ -0,0 +1,13 @@
+package com.twogenius.safetybike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twogenius/safetybike/SafetyBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twogenius.safetybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
